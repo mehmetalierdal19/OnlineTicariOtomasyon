8bf846a8b6bc4513969b9e8096ac97308dd6520f
mehmetalierdal19 <mehmetalierdal26@gmail.com>
2022-08-18 01:44:30 +0300

    model sınıfları eklendi

---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Models/Siniflar/Departman.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Departman
+    {
+        [Key]
+        public int DepartmanID { get; set; }
+        public string DepartmanAd { get; set; }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Models/Siniflar/Urun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Urun
+    {
+        [Key]
+        public int UrunID { get; set; }
+        public string UrunAd { get; set; }
+        public string Marka { get; set; }
+        public int Stok { get; set; }
+        public decimal AlisFiyat { get; set; }
+        public decimal SatisFiyat { get; set; }
+        public bool Durum { get; set; }
+        public string Gorsel { get; set; }
+    }
+}
