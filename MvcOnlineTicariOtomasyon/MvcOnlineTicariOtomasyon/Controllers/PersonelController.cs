d8ad80d24ef5be08375420d93a380b2a4e48ff2e
mehmetalierdal19 <mehmetalierdal26@gmail.com>
2022-08-23 02:30:42 +0300

    peresonel detay görünütleme linki eklendi. personel güncelleme işlemi yapıldı.

--- MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -36,5 +36,27 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult PersonelGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Departmans.Where(a => a.Durum == true).ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmanAd,
+                                               Value = x.DepartmanID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            var personel = c.Personels.Find(id);
+            return View("PersonelGetir", personel);
+        }
+        public ActionResult PersonelGuncelle(Personel p)
+        {
+            var prs = c.Personels.Find(p.PersonelID);
+            prs.PersonelAd = p.PersonelAd;
+            prs.PersonelGorsel = p.PersonelGorsel;
+            prs.PersonelSoyad = p.PersonelSoyad;
+            prs.Departmanid = p.Departmanid;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
