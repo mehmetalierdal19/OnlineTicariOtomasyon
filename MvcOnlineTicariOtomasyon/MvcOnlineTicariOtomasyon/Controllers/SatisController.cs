227567e9e959da3d54eb6821d9a9fba4b6464728
mehmetalierdal19 <mehmetalierdal26@gmail.com>
2022-08-23 03:22:18 +0300

    sayış listeleme ve ekleme işlemi yapıldı

---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/SatisController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class SatisController : Controller
+    {
+        Context c = new Context();
+        // GET: Satis
+        public ActionResult Index()
+        {
+            var deger = c.SatisHarekets.ToList();
+            return View(deger);
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            List<SelectListItem> urun = (from x in c.Uruns.Where(a => a.Durum == true).ToList()
+                                         select new SelectListItem
+                                         {
+                                             Text = x.UrunAd,
+                                             Value = x.UrunID.ToString()
+                                         }).ToList();
+            List<SelectListItem> cari = (from x in c.Carilers.Where(a => a.Durum == true).ToList()
+                                         select new SelectListItem
+                                         {
+                                             Text = x.CariAd + " " + x.CariSoyad,
+                                             Value = x.CariID.ToString()
+                                         }).ToList();
+            List<SelectListItem> personel = (from x in c.Personels.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                                 Value = x.PersonelID.ToString()
+                                             }).ToList();
+            ViewBag.personel = personel;
+            ViewBag.cari = cari;
+            ViewBag.urun = urun;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(SatisHareket sh)
+        {
+            sh.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            c.SatisHarekets.Add(sh);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
