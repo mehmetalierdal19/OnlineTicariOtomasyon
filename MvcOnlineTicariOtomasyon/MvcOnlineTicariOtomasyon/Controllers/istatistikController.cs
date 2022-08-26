f53f6ab47256c8c7d8a64f850a2e8c802c26c583
mehmetalierdal19 <mehmetalierdal26@gmail.com>
2022-08-26 03:33:35 +0300

    istatistik sayfası tasarımı için template eklendi, tasarım yapıldı, bazı veriler için sorgular yazıldı.

---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/istatistikController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class istatistikController : Controller
+    {
+        Context c = new Context();
+        // GET: istatistik
+        public ActionResult Index()
+        {
+            var deger1 = c.Carilers.Where(x => x.Durum == true).Count().ToString();
+            ViewBag.d1 = deger1;
+
+            var deger2 = c.Uruns.Where(x => x.Durum == true).Count().ToString();
+            ViewBag.d2 = deger2;
+
+            var deger3 = c.Personels.Count().ToString();
+            ViewBag.d3 = deger3;
+
+            var deger4 = c.Kategoris.Count().ToString();
+            ViewBag.d4 = deger4;
+            return View();
+        }
+    }
+}
