1eba1d59338c1ab816c0a3cfc8ee00ca208f410b
mehmetalierdal19 <mehmetalierdal26@gmail.com>
2022-08-30 02:10:02 +0300

    galeri blölümünde resim listeleme işlemi ve tema entegresi yapıldı. e-ticaret teması projeye dahil edildi.

---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/GaleriController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class GaleriController : Controller
+    {
+        Context c = new Context();
+        // GET: Galeri
+        public ActionResult Index()
+        {
+            var degerler = c.Uruns.Where(x => x.Durum == true).ToList();
+            return View(degerler);
+        }
+    }
+}
