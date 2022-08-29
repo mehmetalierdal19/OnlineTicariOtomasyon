32117bad66b1121febe8f7e606e72235d53c10fa
mehmetalierdal19 <mehmetalierdal26@gmail.com>
2022-08-29 03:08:44 +0300

    istatistik sayfasındaki istatistikler için linq sorguları yazıldı.

--- MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/istatistikController.cs
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/istatistikController.cs
@@ -24,6 +24,51 @@ namespace MvcOnlineTicariOtomasyon.Controllers
 
             var deger4 = c.Kategoris.Count().ToString();
             ViewBag.d4 = deger4;
+
+            var deger5 = c.Uruns.Sum(x => x.Stok).ToString();
+            ViewBag.d5 = deger5;
+
+            var deger6 = (from x in c.Uruns select x.Marka).Distinct().Count().ToString();
+            ViewBag.d6 = deger6;
+
+            var deger7 = c.Uruns.Count(x => x.Stok <= 20).ToString();
+            ViewBag.d7 = deger7;
+
+            var deger8 = (from x in c.Uruns orderby x.SatisFiyat descending select x.UrunAd).FirstOrDefault();
+            ViewBag.d8 = deger8;
+
+            var deger9 = (from x in c.Uruns orderby x.SatisFiyat ascending where x.Durum == true select x.UrunAd).FirstOrDefault();
+            ViewBag.d9 = deger9;
+
+            var deger10 = c.Uruns.Count(x => x.UrunAd == "Buzdolabı").ToString();
+            ViewBag.d10 = deger10;
+
+            var deger11 = c.Uruns.Count(x => x.UrunAd == "Laptop").ToString();
+            ViewBag.d11 = deger11;
+
+            var deger12 = c.Uruns.GroupBy(x => x.Marka).OrderByDescending(z => z.Count()).Select(y => y.Key).FirstOrDefault();
+            ViewBag.d12 = deger12;
+
+            var deger13 = c.Uruns.Where(u => u.UrunID == (c.SatisHarekets.GroupBy(x => x.Urunid).OrderByDescending(z => z.Count()).Select(y => y.Key).FirstOrDefault())).Select(k => k.UrunAd).FirstOrDefault();
+            ViewBag.d13 = deger13;
+
+            var deger14 = c.SatisHarekets.Sum(x => x.ToplamTutar).ToString();
+            ViewBag.d14 = deger14;
+
+            DateTime bugun = DateTime.Today;
+            var deger15 = c.SatisHarekets.Count(x => x.Tarih == bugun).ToString();
+            ViewBag.d15 = deger15;
+
+            if (Convert.ToInt32(deger15) != 0)
+            {
+                var deger16 = c.SatisHarekets.Where(x => x.Tarih == bugun).Sum(y => y.ToplamTutar).ToString();
+                ViewBag.d16 = deger16;
+            }
+            else 
+            { 
+                ViewBag.d16 = deger15; 
+            }
+
             return View();
         }
     }
